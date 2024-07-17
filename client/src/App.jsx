5c08a77f93a4bbebfc26bea40e13ea1bc73ef2d3
proyectos-developer <proyectos@developer-ideas.com>
2024-07-17 09:33:25 -0500

    V1.0.0

---
+++ client/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import './styles.css'
+
+import GlobalPanel from './components/global/panel.jsx'
+import GlobalPanelTablet from './components/global/paneltablet.jsx'
+import GlobalPanelCell from './components/global/panelcell.jsx'
+
+export default function App() {
+    const [width, setWidth] = useState (window.outerWidth)
+
+    useEffect(() => {
+      window.addEventListener('resize', handle_resize)
+
+      return () => {
+        window.removeEventListener('resize', handle_resize)
+      }
+    }, [])
+
+    const handle_resize = () => {
+      setWidth(window.outerWidth)
+    }
+
+    return (
+      <BrowserRouter>
+          <Routes>
+              <Route path='/' element={width < 500 ? <GlobalPanelCell   proporcional={499 / width}/> : 
+                                       width < 991 ? <GlobalPanelTablet proporcional={991 / width}/> : 
+                                                     <GlobalPanel       proporcional={1920 / width} />}>
+                                                      
+              </Route>
+          </Routes>
+      </BrowserRouter>
+    )
+}
---
+++ client/src/redux/reducers/data.js
@@ -0,0 +1,28 @@
+import {datatypes} from '../actions/data.js'
+
+const initial_state = {
+    authenticated: false,
+    open_menu_main: true,
+    open_modal_new_tarea: false,
+    open_modal_new_modalidad: false
+}
+
+const data = (state = initial_state, action) => {
+    if (action.type === datatypes.SET_AUTHENTICATED){
+        const authenticated = action.authenticated
+        return {
+            ... state,
+            authenticated
+        }
+    }else if (action.type === datatypes.SET_OPEN_MENU_MAIN){
+        const open_menu_main = action.open_menu_main
+        return {
+            ... state,
+            open_menu_main
+        }
+    }else{
+        return state
+    }
+}
+
+export default data
